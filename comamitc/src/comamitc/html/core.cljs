ad2d52cdf620abc05098d5f92168367a7d769da1
Mitch Comardo <mcomardo@cmn.com>
2015-06-29 13:44:31 -0500

    SAVEPOINT

--- comamitc/src/comamitc/html/core.cljs
+++ comamitc/src/comamitc/html/core.cljs
@@ -4,14 +4,30 @@
     [comamitc.utils :as utils]
     [reagent.core :as reagent :refer [atom]]))
 
+(defn body-content [props]
+  [:div#bodyContent.wrapper-a0def "body"])
+
 (defn nav-bar [props]
-  )
+  [:header#navBar.wrapper-a0def.header-7f1e8 
+    [:a.header-left-363bb {:href "https://comamitc.me/"}
+      [:div.header-logo-5bacd ;"</>"]
+        [:img {:src "images/texas-sil.png"
+               :alt "MFC"
+               :height "50px"}]]
+      [:div.header-title-947c2  "Mitch Comardo"]]
+    [:div..header-right-591aa "."]])
+
+(defn nav-bar2 [props]
+  [:header#navBar.wrapper-a0def.header-7f1e8 
+    [:ul 
+      [:li "one"]
+      [:li "two"]
+      [:li "three"]]])
 
 (defn app [props]
-  (utils/log props)
   [:div
-    [:div#navBar "header"]
-    [:div#bodyContent "body"]])
+    (nav-bar2)
+    (body-content)])
 
 (defn ^:export render []
   (reagent/render [app]
